6456ba74f7015a6bdaab0bf6c8252c423bd7b69c
Osvaldo Ochoa <153673366+OsvaldoOchoa30@users.noreply.github.com>
2024-03-19 11:25:28 -0600

    Interfaz Admin

--- ProyectoMultidiciplinario/src/main/java/org/marcosbrindis/proyectomultidiciplinario/controllers/MenuAdminController.java
+++ ProyectoMultidiciplinario/src/main/java/org/marcosbrindis/proyectomultidiciplinario/controllers/MenuAdminController.java
@@ -1,5 +1,104 @@
 package org.marcosbrindis.proyectomultidiciplinario.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class MenuAdminController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button ButtomAgregarProducto;
+
+    @FXML
+    private Button ButtomCancelarPedido;
+
+    @FXML
+    private Button ButtomCrearNuevoUsuario;
+
+    @FXML
+    private Button ButtomCrearPedido;
+
+    @FXML
+    private Button ButtomModificarProducto;
+
+    @FXML
+    private Button ButtomModificarUsuarios;
+
+    @FXML
+    private Button ButtomVentas;
+
+    @FXML
+    private Button ButtomVerMenu;
+
+    @FXML
+    private Button ButtomVerPedidos;
+
+    @FXML
+    void OnMouseClickedAgregarProducto(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseClickedCancelarPedido(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseClickedCrearNuevoUsuario(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseClickedCrearPedido(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseClickedModificarProducto(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseClickedModificarUsuario(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseClickedVentas(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseClickedVerMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseClickedVerPedidos(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert ButtomAgregarProducto != null : "fx:id=\"ButtomAgregarProducto\" was not injected: check your FXML file 'menuAdmin-view.fxml'.";
+        assert ButtomCancelarPedido != null : "fx:id=\"ButtomCancelarPedido\" was not injected: check your FXML file 'menuAdmin-view.fxml'.";
+        assert ButtomCrearNuevoUsuario != null : "fx:id=\"ButtomCrearNuevoUsuario\" was not injected: check your FXML file 'menuAdmin-view.fxml'.";
+        assert ButtomCrearPedido != null : "fx:id=\"ButtomCrearPedido\" was not injected: check your FXML file 'menuAdmin-view.fxml'.";
+        assert ButtomModificarProducto != null : "fx:id=\"ButtomModificarProducto\" was not injected: check your FXML file 'menuAdmin-view.fxml'.";
+        assert ButtomModificarUsuarios != null : "fx:id=\"ButtomModificarUsuarios\" was not injected: check your FXML file 'menuAdmin-view.fxml'.";
+        assert ButtomVentas != null : "fx:id=\"ButtomVentas\" was not injected: check your FXML file 'menuAdmin-view.fxml'.";
+        assert ButtomVerMenu != null : "fx:id=\"ButtomVerMenu\" was not injected: check your FXML file 'menuAdmin-view.fxml'.";
+        assert ButtomVerPedidos != null : "fx:id=\"ButtomVerPedidos\" was not injected: check your FXML file 'menuAdmin-view.fxml'.";
+
+    }
+
 }
 
