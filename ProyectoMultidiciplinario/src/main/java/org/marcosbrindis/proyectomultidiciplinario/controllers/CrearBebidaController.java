d9f7810793719313cb9044bff4de3d84cde838f3
Osvaldo Ochoa <153673366+OsvaldoOchoa30@users.noreply.github.com>
2024-03-22 22:49:41 -0600

    Controladores y asignacion de ID en crearBebida y crearTaco

--- ProyectoMultidiciplinario/src/main/java/org/marcosbrindis/proyectomultidiciplinario/controllers/CrearBebidaController.java
+++ ProyectoMultidiciplinario/src/main/java/org/marcosbrindis/proyectomultidiciplinario/controllers/CrearBebidaController.java
@@ -1,4 +1,63 @@
 package org.marcosbrindis.proyectomultidiciplinario.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class CrearBebidaController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button ButtomCrearBebida;
+
+    @FXML
+    private ComboBox<?> ComboBoxSizeBebida;
+
+    @FXML
+    private ComboBox<?> ComboBoxTipoBebida;
+
+    @FXML
+    private TextField TextFieldDescripcionBebida;
+
+    @FXML
+    private TextField TextFieldNombreBebida;
+
+    @FXML
+    private TextField TextFieldPrecioBebida;
+
+    @FXML
+    void OnMouseClickedButtomCrear(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseClickedComboBoxSizeBebida(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseClickedComboBoxTipoBebida(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert ButtomCrearBebida != null : "fx:id=\"ButtomCrearBebida\" was not injected: check your FXML file 'crearBebida-view.fxml'.";
+        assert ComboBoxSizeBebida != null : "fx:id=\"ComboBoxSizeBebida\" was not injected: check your FXML file 'crearBebida-view.fxml'.";
+        assert ComboBoxTipoBebida != null : "fx:id=\"ComboBoxTipoBebida\" was not injected: check your FXML file 'crearBebida-view.fxml'.";
+        assert TextFieldDescripcionBebida != null : "fx:id=\"TextFieldDescripcionBebida\" was not injected: check your FXML file 'crearBebida-view.fxml'.";
+        assert TextFieldNombreBebida != null : "fx:id=\"TextFieldNombreBebida\" was not injected: check your FXML file 'crearBebida-view.fxml'.";
+        assert TextFieldPrecioBebida != null : "fx:id=\"TextFieldPrecioBebida\" was not injected: check your FXML file 'crearBebida-view.fxml'.";
+
+    }
+
 }
--- ProyectoMultidiciplinario/src/main/java/org/marcosbrindis/proyectomultidiciplinario/controllers/CrearTacoController.java
+++ ProyectoMultidiciplinario/src/main/java/org/marcosbrindis/proyectomultidiciplinario/controllers/CrearTacoController.java
@@ -1,4 +1,54 @@
 package org.marcosbrindis.proyectomultidiciplinario.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class CrearTacoController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button ButtomCrearTaco;
+
+    @FXML
+    private ComboBox<?> ComboBoxTipoCarne;
+
+    @FXML
+    private TextField TextFieldDescripcionTaco;
+
+    @FXML
+    private TextField TextFieldNombreTaco;
+
+    @FXML
+    private TextField TextFieldPrecioTaco;
+
+    @FXML
+    void OnMouseClickedButtomCrearTaco(MouseEvent event) {
+
+    }
+
+    @FXML
+    void OnMouseClickedComboBoxTipoCarne(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert ButtomCrearTaco != null : "fx:id=\"ButtomCrearTaco\" was not injected: check your FXML file 'crearTaco-view.fxml'.";
+        assert ComboBoxTipoCarne != null : "fx:id=\"ComboBoxTipoCarne\" was not injected: check your FXML file 'crearTaco-view.fxml'.";
+        assert TextFieldDescripcionTaco != null : "fx:id=\"TextFieldDescripcionTaco\" was not injected: check your FXML file 'crearTaco-view.fxml'.";
+        assert TextFieldNombreTaco != null : "fx:id=\"TextFieldNombreTaco\" was not injected: check your FXML file 'crearTaco-view.fxml'.";
+        assert TextFieldPrecioTaco != null : "fx:id=\"TextFieldPrecioTaco\" was not injected: check your FXML file 'crearTaco-view.fxml'.";
+
+    }
+
 }
